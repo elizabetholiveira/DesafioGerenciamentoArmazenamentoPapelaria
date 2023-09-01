6d6f1cb51d97f3bc205a326f980b9f380c479447
elizabetholiveira <elizabeth.oliveira@zup.com.br>
2023-08-31 23:13:50 -0300

    terceiro commit

--- stationeryshop/src/main/java/com/challenge/stationeryshop/model/StationeryModel.java
+++ stationeryshop/src/main/java/com/challenge/stationeryshop/model/StationeryModel.java
@@ -1,14 +1,13 @@
 package com.challenge.stationeryshop.model;
 
 import com.challenge.stationeryshop.dto.StationeryDTO;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Entity
@@ -27,19 +26,21 @@ public class StationeryModel {
     private Long id;
 
     //Nome não pode estar em branco
-    @Column(nullable = false)
-    @NotBlank
+    //@Column(nullable = false)
+    @NotNull(message = "name-1")
+    @NotBlank(message = "name-2") //Só esse já faz o trabalho de não deixar branco nem nulo
     private String name;
 
     //Descrição é opcional
     private String description;
 
     //Preço tem que ter 2 casas decimais
-    @Column(nullable = false)
-    @NotBlank
-    @Digits(integer = 9, fraction = 2)
+    //@Column(nullable = false)
+    @NotNull(message = "price-1")
+    @Digits(integer = 9, fraction = 2, message = "price-2")
     private BigDecimal price;
 
     //Quantidade é contada em unidades, então só pode ser inteiro
+    @Min(value = 0, message = "quantity-1")
     private Long quantity;
 }
